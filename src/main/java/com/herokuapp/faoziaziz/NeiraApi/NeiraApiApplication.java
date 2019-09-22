4b0e5bcc206c87d4af3a436b8942843048451c22
Aziz Amerul Faozi <admin@labseni.com>
2019-09-22 18:47:53 +0800

    jem boot

---
+++ src/main/java/com/herokuapp/faoziaziz/NeiraApi/NeiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.faoziaziz.NeiraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeiraApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/herokuapp/faoziaziz/NeiraApi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.herokuapp.faoziaziz.NeiraApi.repository;
+
+
+
+
+import com.herokuapp.faoziaziz.NeiraApi.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
